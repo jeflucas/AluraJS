5cec57df47bc873bca149733e0375d73aafc76b6
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-01 20:37:11 -0300

    CTD

--- js/principal.js
+++ js/principal.js
@@ -32,4 +32,13 @@ btnAdicionar.addEventListener("click", function (event) {
   var peso = form.peso.value;
   var altura = form.altura.value;
   var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+  
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
 });
