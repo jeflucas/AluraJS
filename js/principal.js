216b28c8989ec1ed6248560688f8b8371fd81f9c
jeflucas <jef.lucas@hotmail.co.uk>
2021-07-31 23:08:08 -0300

    CTD

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,14 @@
 var title = document.querySelector(".title");
 title.textContent = "Aparecida Nutrition"
+
+var paciente01 = document.querySelector("#paciente01");
+
+var p1Peso = paciente01.querySelector(".info-peso");
+var peso = p1Peso.textContent;
+
+var p1Altura = paciente01.querySelector(".info-altura");
+var altura = p1Altura.textContent;
+
+var imc = peso / (altura * altura);
+
+paciente01.querySelector(".info-imc").textContent = imc;
