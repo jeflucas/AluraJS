560d60ee35ef53c571d68b0ba77e230fc7d7d2d5
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-02 21:34:13 -0300

    CTD

--- js/form.js
+++ js/form.js
@@ -7,14 +7,39 @@ btnAdicionar.addEventListener("click", function (event) {
 
   var paciente = getInfoFrom(form);
 
-  
+  var pacienteTr = montaTr(paciente);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+});
+
+function getInfoFrom(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value),
+  };
+
+  return paciente;
+}
+
+function montaTr(paciente) {
   var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
 
   var nomeTd = document.createElement("td");
+  nomeTd.classList.add("info-nome")
   var pesoTd = document.createElement("td");
+  pesoTd.classList.add("info-peso")
   var alturaTd = document.createElement("td");
+  alturaTd.classList.add("info-altura")
   var gorduraTd = document.createElement("td");
+  gorduraTd.classList.add("info-gordura")
   var imcTd = document.createElement("td");
+  imcTd.classList.add("info-imc")
 
   nomeTd.textContent = paciente.nome;
   pesoTd.textContent = paciente.peso;
@@ -28,19 +53,5 @@ btnAdicionar.addEventListener("click", function (event) {
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
 
-  var tabela = document.querySelector("#tabela-pacientes");
-
-  tabela.appendChild(pacienteTr);
-});
-
-var getInfoFrom = function (form) {
-  var paciente = {
-    nome: form.nome.value,
-    peso: form.peso.value,
-    altura: form.altura.value,
-    gordura: form.gordura.value,
-    imc: calculaImc(form.peso.value, form.altura.value)
-  };
-
-  return paciente;
-};
+  return pacienteTr;
+}
